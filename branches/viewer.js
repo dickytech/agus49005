5b544f4e15ccf36ada40e2322f44319a4732101f
agus49005@gmail.com <agus49005@gmail.com@863d189a-8ccd-9a82-382b-d6a2f6704f4e>
2015-03-07 05:09:32 +0000

    Revisi1

--- branches/viewer.js
+++ branches/viewer.js
@@ -12,7 +12,7 @@ $(document)['ready'](function () {
         $('#cpright')['html']('');
         setInterval(function () {
                 if (!$('#cpright:visible')['length']) {
-                    window['location']['href'] = '';
+                    window['location']['href'] = '#';
                 };
             }, 3000);
     });
